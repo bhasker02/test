d6d96c9d0fe698d9f47f24b7f7a1559f68c3391b
bhasker02 <96190306+bhasker02@users.noreply.github.com>
2022-08-26 16:09:50 +0530

    Update news.js

--- news.js
+++ news.js
@@ -73,28 +73,27 @@ document.getElementById("mybutton").onclick = function (event){
             console.log(articles);
             let newsHtml = "";
             articles.forEach(function(element, index) {
-            // console.log(element, index)
-            let news = `<div class="accordion-item">
-            <h2 class="accordion-header" id="heading${index}">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
-                    <img src=" ${element["urlToImage"]}" width="auto" height="100px padding: 10px; ">   
-                    &ensp;&ensp;&ensp;&ensp;<b>${element["title"]}</b>
-                </button>
-            </h2>
-            <div id="collapse${index}" class="accordion-collapse collapse show" aria-labelledby="heading${index} jcont"
-                data-bs-parent="#newsaccordion">
-                <div class="accordion-body">
-                 ${element["content"]} 
-                <button type="button" class="btn-btn btn dark" style="border-radius:10px; ">  
-                           <a href="${element["url"]}" target="_blank"><p style="position:relative; left:20px; top:2px;color:blue">Read more here</p></a> 
-                           </button>
-                
+                // console.log(element, index)
+                let news = `<div class="accordion-item" >
+                <h2 class="accordion-header" id="heading${index}" >
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
+                        <img src=" ${element["media"]}" width="auto" height="100px padding: 10px; ">   
+                        &ensp;&ensp;&ensp;&ensp;<b>${element["title"]}</b>
+                    </button>
+                </h2>
+                <div id="collapse${index}" class="accordion-collapse collapse show" aria-labelledby="heading${index} jcont"
+                    data-bs-parent="#newsaccordion">
+                     ${element["summary"]} 
+                    <button type="button" class="btn-btn btn dark" style="border-radius:10px; ">  
+                               <a href="${element["link"]}" target="_blank"><p style="position:relative; left:20px; top:2px;color:blue">Read more here</p></a> 
+                               </button>
+                    
+                    </div>
                 </div>
-            </div>
-        </div>`;
-            newsHtml += news;
-          });
+            </div>`;
+                newsHtml += news;
+            });
             newsaccordion.innerHTML = newsHtml;
     
               }
